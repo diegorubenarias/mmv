d63453a634532d6226987c67753374c6d379d52b
diego <darias@marketkeytech.com>
2022-08-31 19:24:29 -0300

    package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { ClubDashboardComponent } from './components/club-dashboard/club-dashboard.component';
-import { RouterModule } from '@angular/router';
 import { CardModule } from "primeng/card";
-import { HttpClient } from "@angular/common/http";
 import { ButtonModule } from "primeng/button";
 import { StepsModule } from "primeng/steps";
 import { InputTextModule } from "primeng/inputtext";
 import { DropdownModule } from "primeng/dropdown";
 import { InputTextareaModule } from "primeng/inputtextarea";
 import { TableModule } from "primeng/table";
-import { FormsModule } from "@angular/forms";
 import { MessagesModule } from "primeng/messages";
 import { MessageModule } from "primeng/message";
 import { TooltipModule } from "primeng/tooltip";
 import { FileUploadModule } from "primeng/fileupload";
-import { HttpClientModule } from "@angular/common/http";
 import { CheckboxModule } from "primeng/checkbox";
 import { CalendarModule } from "primeng/calendar";
 import { RadioButtonModule } from "primeng/radiobutton";
 import { DialogModule } from "primeng/dialog";
 import { ConfirmDialogModule } from "primeng/confirmdialog";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -51,7 +50,6 @@ import { ProgressSpinnerModule } from "primeng/progressspinner";
     FormsModule,
     TooltipModule,
     FileUploadModule,
-    HttpClientModule,
     CheckboxModule,
     CalendarModule,
     RadioButtonModule,
@@ -59,6 +57,7 @@ import { ProgressSpinnerModule } from "primeng/progressspinner";
     ConfirmDialogModule,
     ProgressSpinnerModule,
     DialogModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
